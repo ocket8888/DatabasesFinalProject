c271f9972d19acb3f54d10a23d293c3f7735790a
Marek Prussak <marek@theprussaks.com>
2017-05-04 14:29:39 -0600

    I dont wanna talk about it

--- import.py
+++ import.py
@@ -1,45 +1,26 @@
 #Script to import data from the data file (response.csv) to the database
 
-import psycopg2
-from psycopg2.extensions import AsIs
+import pandas as ps
+from sqlalchemy import create_engine
 
-#open the file in read only mode. assumes same directory
-file = open('sanitized_responses.csv', 'r')
+file = 'responses.csv'
+table = 'responses'
+conn_str = 'postgresql://marek@localhost/final_project'
 
-#get all the column names and sanitize inputs
-#I'm not sorry about the following line, sue me (yes, chained replaces are supposedly the fastest)
-# actually this kept growing so now I feel a little sorry but not enough to change it
-f_columns = file.readline().lower().split(',')
+#read csv into a dataframe
+responses = ps.read_csv(file)
 
-#open a connection to the database. this *should* only work from the local machine
-# as the server is configured to require passwords for connections from other machines.
-conn = psycopg2.connect("dbname=final_project user=marek")
+#strip commas and single quotes
+responses.columns = responses.columns.str.replace(',|\'', '')
 
-#create a cursor, the object that actually interacts with the database
-cur = conn.cursor()
+#convert every character to lowercase
+responses.columns = responses.columns.str.lower()
 
-#delete the table if it exists
-cur.execute("DROP TABLE IF EXISTS responses;")
+#replace pseudo-whitespace with underscores
+responses.columns = responses.columns.str.replace(' |-|/', '_')
 
-#create a table with one row, just a serial id to serve as the primary key
-#it's worth noting the use of transactions, so nothing is permanent until you
-# call conn.execute() and if something fails, you'll have to call conn.rollback()
-# and start over
-cur.execute("CREATE TABLE responses (id SERIAL PRIMARY KEY);")
+#create engine for db connection
+engine = create_engine(conn_str)
 
-#add all the columns to the table. short of hardcoding each column, or looking at the next line,
-# I couldn't figure out an easy way to distinguish between text and integer columns. so for now
-# everything is text, if it turns out to be a significant problem it shouldn't be terribly difficult
-# to change.
-
-for column in f_columns:
-	#the second parameter has to be a single-item tuple because *reasons*
-	#need the AsIs to prevent the addition of unwanted quotes
-	cur.execute("ALTER TABLE responses ADD %s text;", [AsIs(column)])
-
-#finally, add the actual data. this shits the easy part
-cur.copy_from(file, 'responses', sep=',', columns=f_columns)
-
-conn.commit()
-cur.close()
-conn.close()
+#run the import
+responses.to_sql('responses', engine, if_exists='replace')
